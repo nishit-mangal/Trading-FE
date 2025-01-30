88900b9446c41fb508e8468146a1bc7901cf17e0
Nishit Mangal <nishitmangal5@gmail.com>
2025-01-30 16:30:17 +0530

    Setting main page css

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -157,97 +157,97 @@ export const Login = () => {
                 <div className="items-center my-2 font-light text-xs">Loading...</div>
             )}
             <div className="flex justify-center w-3/5 max-w-2xl">
-                <div className="flex flex-col justify-start w-5/12 p-8 bg-white shadow-teal-600 border rounded-lg ">
-                    <div className="text-2xl font-sans font-semibold text-left">Welcome to</div>
-                    <div className="text-5xl font-serif font-bold text-teal-800">NIVESH</div>
-                    <img src={wizard} alt="Wizard" width={120} className="my-4"></img>       
-                    <div className="mt-2 text-base font-mono">Aapka apna</div> 
-                    <div className="mt-1 text-3xl font-serif">Portfolio Manager</div> 
+                <div className="flex flex-col justify-start w-5/12 p-5 bg-teal-700 border rounded-lg ">
+                    <div className="text-2xl font-sans font-semibold text-left text-teal-200">Welcome to</div>
+                    <div className="text-5xl font-serif font-bold text-white">NIVESH</div>
+                    <img src={wizard} alt="Wizard" width={150} className="my-4 bg-white"></img>       
+                    <div className="text-base font-mono font-semibold">Aapka apna</div> 
+                    <div className="text-3xl font-serif font-bold text-teal-50">Portfolio Manager</div> 
                 </div>
                 <GoogleOAuthProvider>
-                <div className="flex flex-col justify-center w-7/12 p-8 bg-white shadow-teal-600 border rounded-lg">
-                    {isAccountVerified === -1 && (
-                        <div>
-                            <div className="text-2xl font-sans font-semibold">Log in</div>
-                            <button className="flex items-center mt-6 mb-4 h-10 w-full px-4 bg-teal-600 text-white text-base font-semibold rounded-md shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50" onClick={loginViaGoogle}>
-                                <img className="rounded-sm mr-2" src={googleImg} alt="Google" height={25} width={25}></img>
-                                <div className="text-center w-full">Login with Google</div>
-                            </button>
-                            <hr></hr>
-
-                            <form onSubmit={handleSubmit} className="pt-1" method="post">
-                                {/* Email Input */}
-                                <div className="form-group mt-2">
-                                    <label
-                                        htmlFor="email"
-                                        className="block text-xs font-medium text-gray-500"
-                                    >
-                                        Email
-                                    </label>
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                        className="font-serif text-xs mt-1 block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
-                                        required
-                                    />
-                                </div>
-
-                                {/* Password Input */}
-                                <div className="form-group mt-1 mr-1">
-                                    <label
-                                        htmlFor="password"
-                                        className="block text-xs font-medium text-gray-500"
-                                    >
-                                        Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        value={formData.password}
-                                        onChange={handleChange}
-                                        className="text-xs mt-1 block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
-                                        required
-                                    />
-                                </div>
-                                <div
-                                    className="text-xs mt-1 text-teal-600 font-medium hover:cursor-pointer font-sans"
-                                    onClick={handleForgotPassword}
-                                >
-                                    Forgot your password?
-                                </div>
-
-                                {/* Submit Button */}
-                                <button
-                                    type="submit"
-                                    className="mt-4 h-8 w-full px-4 bg-teal-600 text-white text-sm font-semibold rounded-md shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"
-                                >
-                                    Login
+                    <div className="flex flex-col justify-center w-7/12 p-8 bg-white shadow-teal-600 border rounded-lg">
+                        {isAccountVerified === -1 && (
+                            <div>
+                                <div className="text-2xl font-sans font-semibold">Log in</div>
+                                <button className="flex items-center mt-6 mb-4 h-10 w-full px-4 bg-teal-600 text-white text-base font-semibold rounded-md shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50" onClick={loginViaGoogle}>
+                                    <img className="rounded-sm mr-2" src={googleImg} alt="Google" height={25} width={25}></img>
+                                    <div className="text-center w-full">Login with Google</div>
                                 </button>
-                            </form>
-                            <div className="text-xs text-center mt-3">
-                                Don't have an account?
-                                <span
-                                    className="ml-2 text-teal-700 font-bold hover:cursor-pointer font-sans"
-                                    onClick={handleCreateAccount}
-                                >
-                                    Create Account
-                                </span>
+                                <hr></hr>
+
+                                <form onSubmit={handleSubmit} className="pt-1" method="post">
+                                    {/* Email Input */}
+                                    <div className="form-group mt-2">
+                                        <label
+                                            htmlFor="email"
+                                            className="block text-xs font-medium text-gray-500"
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                            className="font-serif text-xs mt-1 block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+                                            required
+                                        />
+                                    </div>
+
+                                    {/* Password Input */}
+                                    <div className="form-group mt-1 mr-1">
+                                        <label
+                                            htmlFor="password"
+                                            className="block text-xs font-medium text-gray-500"
+                                        >
+                                            Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            name="password"
+                                            value={formData.password}
+                                            onChange={handleChange}
+                                            className="text-xs mt-1 block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+                                            required
+                                        />
+                                    </div>
+                                    <div
+                                        className="text-xs mt-1 text-teal-600 font-medium hover:cursor-pointer font-sans"
+                                        onClick={handleForgotPassword}
+                                    >
+                                        Forgot your password?
+                                    </div>
+
+                                    {/* Submit Button */}
+                                    <button
+                                        type="submit"
+                                        className="mt-4 h-8 w-full px-4 bg-teal-600 text-white text-sm font-semibold rounded-md shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"
+                                    >
+                                        Login
+                                    </button>
+                                </form>
+                                <div className="text-xs text-center mt-3">
+                                    Don't have an account?
+                                    <span
+                                        className="ml-2 text-teal-700 font-bold hover:cursor-pointer font-sans"
+                                        onClick={handleCreateAccount}
+                                    >
+                                        Create Account
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                    )}
-                    {isAccountVerified === 0 && (
-                        <Otp
-                            email={otpEmail}
-                            setResponseErr={setResponseErr}
-                            page="Login"
-                            setVerified={setIsAccountVerified}
-                        />
-                    )}
-                </div>
+                        )}
+                        {isAccountVerified === 0 && (
+                            <Otp
+                                email={otpEmail}
+                                setResponseErr={setResponseErr}
+                                page="Login"
+                                setVerified={setIsAccountVerified}
+                            />
+                        )}
+                    </div>
                 </GoogleOAuthProvider>
             </div>
         </div>
